1935ad303c895141e722fe94d23db7892d65a26d
AiSonu-patel <aisonupatel09@gmail.com>
2024-05-27 00:54:55 +0530

    Modified

--- 09_Recursion/32_Predict-the-winner.cpp
+++ 09_Recursion/32_Predict-the-winner.cpp
@@ -32,10 +32,23 @@ int winner(vector<bool>&persion, int n, int index, int persion_left, int k)
     return winner(persion, n, index, persion_left-1, k);
 }
 
+// T.C -> O(n)
+// S.C -> O(n)
+
+int Winner(int n, int k)
+{
+    if(n==1)
+    return 0;
+
+    return (Winner(n-1, k)+k)%n;
+}
+
 int main()
 {
-    int n=5;
-    vector<bool>persion(n,0);
-    int kill = 3;
-    cout<<winner(persion, n, 0, n, kill); // 3
+    int n=16;
+    // vector<bool>persion(n,0);
+    // int kill = 3;
+    // cout<<winner(persion, n, 0, n, kill); // 3
+
+    cout<<Winner(n, 2)+1; // 3
 }
