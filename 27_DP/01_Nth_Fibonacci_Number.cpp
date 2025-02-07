6c5f25bb033f31d86e0ba7b66e9aa77391fc21b7
AiSonu-patel <aisonupatel09@gmail.com>
2025-02-07 12:07:37 +0530

    Add

--- 27_DP/01_Nth_Fibonacci_Number.cpp
+++ 27_DP/01_Nth_Fibonacci_Number.cpp
@@ -26,9 +26,8 @@ int solve(int n, vector<int> &dp)
     if (dp[n] != -1)
         return dp[n];
 
-    dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
 
-    return dp[n];
 };
 
 // Nth Fibonacci Number solve using dp (bottom up approach)
@@ -37,14 +36,14 @@ int solve(int n, vector<int> &dp)
 
 int solution(int n)
 {
-    vector<int>dp(n+1);
+    vector<int>dp(n+1, -1);
     dp[0]=0;
     dp[1]=1;
 
     for(int i=2; i<=n; i++)
-    dp[i] = dp[i-1]+dp[i-2];
+    return dp[i] = dp[i-1]+dp[i-2];
 
-    return dp[n];
+    // return dp[n];
 }
 
 // Nth Fibonacci Number solve space optimization
--- 27_DP/02_Climbing_Stairs.cpp
+++ 27_DP/02_Climbing_Stairs.cpp
@@ -1,20 +1,127 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int step(int stair, int n)
+
+// recursion 
+// Time Complexity (2^n)
+// Space Complexity (n)
+int count(int i, int n)
 {
-    if(stair==n)
+    if(i==n)
     return 1;
 
-    if(stair>n)
+    if(i>n)
     return 0;
 
-    return step(stair+1, n)+step(stair+2, n);
+    return count(i+1, n)+count(i+2, n);
+}
+
+int count2(int n)
+{
+    if(n<=1)
+    return 1;
+
+    return count2(n-1)+count2(n-2);
+}
+
+// Climbing Stairs solve using recursion + dp (top down approach)
+// T.C -> O(n)
+// S.C -> O(2n) -> O(n)
+
+int topDownApproach(int i, int n, vector<int>&dp)
+{
+    if(i==n)
+    return 1;
+
+    if(i>n)
+    return 0;
+
+    if(dp[i]!=-1)
+    return dp[i];
+
+    return dp[i] = topDownApproach(i+1, n, dp)+topDownApproach(i+2, n, dp);
 };
+
+int topDownApproach2(int n, vector<int>&dp)
+{
+    if(n<=1)
+    return 1;
+
+    if(dp[n] != -1)
+    return dp[n];
+
+    return dp[n] = topDownApproach2(n-1, dp)+topDownApproach2(n-2, dp);
+};
+
+// Nth Fibonacci Number solve using dp (bottom up approach)
+// T.C -> O(n)
+// S.C -> O(n);
+
+int bottomUpApproach(int n)
+{
+    vector<int>dp(n+1, -1);
+    dp[n+1] = 0;
+    dp[n] = 1;
+    
+    for(int i=n-1; i>=0; i--)
+    {
+        dp[i] = dp[i+1]+dp[i+2];
+    }
+
+    return dp[0];
+}
+
+int bottomUpApproach2(int n)
+{
+    vector<int>dp(n+1, -1);
+    dp[1] = 1;
+    dp[0] = 1;
+
+    for(int i=2; i<=n; i++)
+    dp[i] = dp[i-1]+dp[i-2];
+
+    return dp[n];
+}
+
+// Nth Fibonacci Number solve space optimization
+// T.C -> O(n)
+// S.C -> O(1)
+
+int spaceOptimization(int n)
+{
+    int next2 = 0;
+    int next = 1;
+    int curr;
+
+    for(int i=n-1; i>=0; i--)
+    {
+        curr = next2+next;
+        next2 = next;
+        next = curr;
+    }
+
+    return curr;
+
+}
+
+int spaceOptimization2(int n)
+{
+    int second = 1, first = 1, curr = 1;
+
+    for(int i=2; i<n+1; i++)
+    {
+        curr = second+first;
+        first = second;
+        second = curr;
+    }
+
+    return curr;
+}
 
 int climbStairs(int n)
 {
-    return step(0, n);
+    return spaceOptimization2(n);
 };
 
 int main()
